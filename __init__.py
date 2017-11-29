6c692f0f33a07e0e4cc2c582ca0d75be40198d5b
sebastienValade <valade.sebastien@gmail.com>
2017-11-29 19:18:09 +0100

    major modifications

--- __init__.py
+++ __init__.py
@@ -3,8 +3,12 @@ from flask import render_template, request, send_from_directory
 from flask.ext.bootstrap import Bootstrap
 import records
 import datetime
+import pandas as pd
+import json
+import plotly
+import plotly.graph_objs as go
+from sqlalchemy import create_engine  # database connection
 
-#
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
@@ -12,14 +16,17 @@ bootstrap = Bootstrap(app)
 # --- connect to database
 db_host = '127.0.0.1'
 db_usr = 'root'
-db_pwd = 'wave'
+db_pwd = 'br12Fol!'
 db_type = 'mysql'
 db_name = 'DB_MOUNTS'
 db_url = db_type + '://' + db_usr + ':' + db_pwd + '@' + db_host + '/' + db_name
 dbo = records.Database(db_url)
 
 
+disk_engine = create_engine(db_url)
+
 # === define routes
+
 
 @app.route('/')
 def index():
@@ -54,11 +61,6 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/debug")
-def debug():
-    return "Berlin!"
-
-
 @app.route("/volcano/<id>")
 def volcano(id):
 
@@ -66,19 +68,19 @@ def volcano(id):
     stmt = "SELECT * FROM DB_MOUNTS.targets WHERE id = '" + id + "'"
     tgt_stats = dbo.query(stmt)
 
-    # --- get target results (ifg+coh)
+    # --- get target s1 results (ifg+coh)
     q = '''
         SELECT R.title, R.abspath, R.type, A.acqstarttime
         FROM results_img AS R
         INNER JOIN archive AS A
-        ON R.id_master = A.id
+        ON R.id_slave = A.id
         WHERE R.target_id = {} AND R.type = 'ifg' OR R.type = 'coh'
         ORDER BY A.acqstarttime desc
         '''
     stmt = q.format(id)
     imgS1 = dbo.query(stmt)
 
-    # --- get target results (ifg+coh)
+    # --- get target s2 results (nir)
     q = '''
         SELECT R.title, R.abspath, R.type, A.acqstarttime
         FROM results_img AS R
@@ -91,6 +93,93 @@ def volcano(id):
     imgS2 = dbo.query(stmt)
 
     return render_template("volcano.html", imgS1=imgS1, imgS2=imgS2, tgt_stats=tgt_stats[0])
+
+
+@app.route("/timeseries/<id>")
+def timeseries(id):
+
+    # --- get target information (display in table)
+    stmt = "SELECT * FROM DB_MOUNTS.targets WHERE id = '" + id + "'"
+    tgt_stats = dbo.query(stmt)                 # =>  name = tgt_stats[0].name
+    # tgt_stats = pd.read_sql(stmt, disk_engine)  # =>  name = tgt_stats.name[0]
+
+    # --- get data in database (pandas/sqlalchemy)
+    # TODO: make a unique query and filter result
+    stmt = "SELECT time, data FROM DB_MOUNTS.results_dat WHERE type='coh' ORDER BY time ASC"
+    df = pd.read_sql(stmt, disk_engine)
+
+    stmt = "SELECT time, data FROM DB_MOUNTS.results_dat WHERE type='nir' ORDER BY time ASC"
+    df_nir = pd.read_sql(stmt, disk_engine)
+
+    # --- plot (pandas)
+    # df.plot(x='time', y='data')
+    # plt.show()
+
+    # -- create the plotly data structure
+    trace1 = go.Scatter(
+        x=df["time"],
+        y=df["data"],
+        name="coh",
+        mode='lines+markers'
+    )
+    trace2 = go.Scatter(
+        x=df_nir["time"],
+        y=df_nir["data"],
+        name="nir",
+        mode='lines+markers'
+    )
+
+    graph = dict(
+        data=[trace1, trace2],
+        layout=dict(
+            yaxis=dict(title="index of change"),
+            showlegend=True
+        )
+    )
+
+    # --- convert the figures to JSON
+    graphJSON = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('timeseries.html', graphJSON=graphJSON, tgt_stats=tgt_stats[0])
+
+
+@app.route("/plot_tmp")
+def plot_tmp():
+    # --- get data in database
+    stmt = "SELECT time, data FROM DB_MOUNTS.results_dat WHERE type='coh' ORDER BY time ASC"
+    rows = dbo.query(stmt)
+    dat = rows.dataset
+    t_str = dat.get_col(0)
+    print t_str
+    # t_datetime = pd.to_datetime(t_str)
+    import datetime
+    t_datetime = [datetime.datetime.strptime(t, "%Y-%m-%dT%H:%M:%S") for t in t_str]
+    y = dat.get_col(1)
+
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from cStringIO import StringIO
+    import base64
+
+    df = pd.DataFrame(
+        {'x': t_datetime, 'y': y},
+    )
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    # plt.plot(t_datetime, y)
+    # plt.xticks(rotation=10)
+    # plt.draw()
+
+    df.plot(ax=ax)
+
+    io = StringIO()
+    fig.savefig(io, format='png')
+    data = base64.encodestring(io.getvalue())
+
+    return html.format(data)
 
 
 # === define additional jinja filters
@@ -132,8 +221,8 @@ html = '''
 '''
 
 
-@app.route("/plot")
-def plot():
+@app.route("/plot_static")
+def plot_static():
     import matplotlib
     matplotlib.use('Agg')
 
